ab3fe9dcc4b438577e0e584e45b08173b60c79d6
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-14 22:29:22 +0530

    taking input from the user

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
     public static void main(String[] args){
         Student obj = new Student();
+        obj.get_details();
         obj.show_details();
     }
 }
--- Student.java
+++ Student.java
@@ -1,9 +1,19 @@
-public class Student{
-    String name = "Bob";
-    int marks = 75;
+import java.util.Scanner;
 
+public class Student{
+    Scanner scan = new Scanner(System.in);
+    String name = "";
+    int marks = 0;
+
+    public void get_details(){//taking input from the user
+        
+        System.out.println("Enter your Name ");
+        name=scan.nextLine();
+        System.out.println("Enter your Marks ");
+        marks = scan.nextInt();
+    }
     public void show_details(){
-        System.out.println("Name: "+name);
-        System.out.println("Marks "+marks);
+        System.out.println("Name of the Student :"+name);
+        System.out.println("Marks of the Student :"+marks);
     }
 }
