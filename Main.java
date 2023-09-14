0312600c87acf15ad63029454d9d0c04c88cd4ef
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-14 19:27:59 +0530

    adding my first java files Main class student class

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        Student obj = new Student();
+        obj.show_details();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    String name = "Bob";
+    int marks = 75;
+
+    public void show_details(){
+        System.out.println("Name: "+name);
+        System.out.println("Marks "+marks);
+    }
+}
