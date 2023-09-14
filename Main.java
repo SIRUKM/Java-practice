ca0d0f78ad8917ae462ec3ffbaeea02c51187d25
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-14 22:42:34 +0530

    constructors

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;//to take input for constructor
 public class Main{
     public static void main(String[] args){
-        Student obj = new Student();
-        obj.get_details();
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String name=scan.nextLine();
+
+        System.out.println("Enter your marks :");
+        int marks  =scan.nextInt();
+        
+        
+        
+        Student obj = new Student(name,marks);
+        // obj.get_details();
         obj.show_details();
     }
 }
--- Student.java
+++ Student.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Student{
-    Scanner scan = new Scanner(System.in);
-    String name = "";
-    int marks = 0;
+    // Scanner scan = new Scanner(System.in);
+    String name;
+    int marks;
 
-    public void get_details(){//taking input from the user
-        
-        System.out.println("Enter your Name ");
-        name=scan.nextLine();
-        System.out.println("Enter your Marks ");
-        marks = scan.nextInt();
+    public Student(String n, int m){//creating constructor
+        name = n;
+        marks = m;
     }
-    public void show_details(){
+
+    // public void get_details(){//taking input from the user
+        
+    //     System.out.println("Enter your Name ");
+    //     name=scan.nextLine();
+    //     System.out.println("Enter your Marks ");
+    //     marks = scan.nextInt();
+    // }
+    public void show_details(){//method
         System.out.println("Name of the Student :"+name);
         System.out.println("Marks of the Student :"+marks);
     }
