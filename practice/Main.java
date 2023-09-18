62852214b88af39e394efc6b2994d98d9ff66703
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-19 02:04:28 +0530

    Using Method

--- practice/Main.java
+++ practice/Main.java
@@ -1,31 +1,52 @@
 package practice;
 class Animal{
 
+    String name;
+    int age;
 
-    public void run(){
-        System.out.println("runing");
+
+    // using Method
+    void initizlize_Variable(String s, int a){
+        name = s;
+        age = a;
     }
 
-    public void eat(){
-    System.out.println("rice is the stable food for most of the indians");
+    void display(){
+        System.out.println("name: "+name+" "+"age :"+age);
     }
+    // public void run(){
+    //     System.out.println("runing");
+    // }
+
+    // public void eat(){
+    // System.out.println("rice is the stable food for most of the indians");
+    // }
 }
 public class Main {
     public static void main(String[] args){
-        System.out.println("1");
+        // System.out.println("1");
         Animal dog = new Animal();
-        dog.run();
-        dog.eat();
+        dog.initizlize_Variable("hg",45);
+        // dog.run();
+        // dog.eat();
 
-        Bird chidiBird = new Bird();
-        chidiBird.fly();
+        // Bird chidiBird = new Bird();
+        // chidiBird.fly();
+
+        // dog.name = "Kamlesh";//By using Re frence Variable
+        // dog.age = 25;//By using Refrence Variable
+
+        // System.out.println(dog.name+"  " +dog.age);
+
+
+        dog.display();
     }
 }
 
 
-class Bird{
+// class Bird{
 
-    public void fly(){
-        System.out.println("I can fly");
-    }
-}
+//     public void fly(){
+//         System.out.println("I can fly");
+//     }
+// }
