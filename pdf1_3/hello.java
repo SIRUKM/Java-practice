ae390c804dad10a0a8d68461fe392e8bf2d9af05
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-29 09:35:41 +0530

    practicing pdf 1.3

---
+++ pdf1_3/hello.java
@@ -0,0 +1,10 @@
+package pdf1_3;
+
+public class hello {
+    public static void main(String[] args) {
+        char ch = 'A';
+        System.out.println(ch);
+        ch++;
+        System.out.println(ch);
+    }
+}
